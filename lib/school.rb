6cd8c19c4bdae219520ed4d52b72782baf9beafc
Taylor Donlan <taylordonlan@gmail.com>
2018-03-07 21:02:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,8 +13,8 @@ class School
   def add_student(name, grade)
     if @roster[grade] != nil
       @roster[grade] << name
-  #  else
-  #    @roster[grade] = [name]
+    else
+      @roster[grade] = [name]
     end
   end
 
